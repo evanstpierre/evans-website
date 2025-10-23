224550cba70c0106447f285d6d35a68d741c35f9
evanstpierre <evanlstpierre@gmail.com>
2025-10-23 16:30:13 -0400

    update for cloudflare

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+ 
 
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },   // ✅ don’t fail the build on ESLint errors
   // typescript: { ignoreBuildErrors: true }, // (optional) if TS errors block builds later
 };
 
-
-export default nextConfig;
+export default defineCloudflareConfig({
+  incrementalCache: r2IncrementalCache,
+});
