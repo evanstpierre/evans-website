d37c5ffa44050135d2355b90b42e733f31a66c83
evanstpierre <evanlstpierre@gmail.com>
2025-10-28 16:18:09 -0400

    add github action

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
-const nextConfig = {
+// next.config.ts
+import { paraglide } from "@inlang/paraglide-js-adapter-next/plugin";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  distDir: "out",
   eslint: {
-    // Don’t run ESLint during builds
+    // Skip ESLint checks during CI builds
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+export default paraglide({
+  paraglide: {
+    project: "./project.inlang",
+    outdir: "./src/paraglide",
+  },
+  ...nextConfig,
+});
