5f86461f0f483d82e447c0865939925d3b119d16
evanstpierre <evanlstpierre@gmail.com>
2025-10-29 13:55:10 -0400

    fix config and wrangler (smh))

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,10 @@
 // next.config.ts
-import { paraglide } from "@inlang/paraglide-js-adapter-next/plugin";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "out",
-  eslint: {
-    // Skip ESLint checks during CI builds
-    ignoreDuringBuilds: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
 };
 
-export default paraglide({
-  paraglide: {
-    project: "./project.inlang",
-    outdir: "./src/paraglide",
-  },
-  ...nextConfig,
-});
+export default nextConfig;
